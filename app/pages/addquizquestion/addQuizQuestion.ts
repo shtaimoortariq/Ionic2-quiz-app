b7b2d5045bb19dce27b0c2b9bd0b5ab753f0823d
taimoor_t <taimoor_t@yahoo.com>
2016-06-26 16:49:54 +0500

    Options are working

--- app/pages/addquizquestion/addQuizQuestion.ts
+++ app/pages/addquizquestion/addQuizQuestion.ts
@@ -3,17 +3,32 @@ import {NavController, NavParams} from 'ionic-angular';
 import {QuizDetailService} from '../../services/quizDetailService';
 
 @Component({
-    templateUrl: 'build/pages/addquizquestion/addQuizQuestion.html'
-
+    templateUrl: 'build/pages/addquizquestion/addquizquestion.html'
 })
 
 export class AddQuizQuestion {
-    quizName: string;
-    quizQuestions: number;
+    quizName           : string;
+    noOfQuizQuestions  : number;
+    quizQuestion       : string;
+    options: any[]     = [];
+    
+
     constructor(private params: NavParams) {
         this.quizName = this.params.data.quizName;
-        this.quizQuestions = this.params.data.quizQuestion;
-        console.log(this.quizName);
-        console.log(this.quizQuestions);
+        this.noOfQuizQuestions = this.params.data.quizQuestion;
+        this.options.push({
+            option: null
+        });
      }
+
+     addQuizQuestions() {
+          this.options.push({
+            option: null
+        });         
+     }
+    
+    submitQuizQuestion() {
+        console.log(this.options);
+    }
+
 }
